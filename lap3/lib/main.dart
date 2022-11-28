335dbd1f05146a2ec66eaf4472bc6dc9eeef8ea3
abdallhx2 <3bdallhx2@gmail.com>
2022-11-28 09:49:03 +0300

    done
    i add some animation in the lap
    check it!

--- lap3/lib/main.dart
+++ lap3/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,72 +34,53 @@ class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      alignment: Alignment.center,
+      alignment: Alignment.bottomRight,
       children: [
-   
-                          Container(
-
-                              decoration: BoxDecoration(
-                               borderRadius: BorderRadius.all( Radius.circular(36),),
-               
-                    
-                              image: DecorationImage(
-                                image: AssetImage('assets/5.png'),
-                              ),
-                              
-                            ),
-                            
-                          ),
-                       
-                          
-                        
         Column(
-           children:[
-            
-                      
+          children: [
             Expanded(
                 child: Row(
               children: [
-               Container(
-                  child: 
-                    Expanded(
-                      child: Container(
-                        color: Color.fromARGB(132, 13, 10, 200),
-                        child: Container(
-                            decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/3.png'),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                ),
-           
                 Expanded(
                   child: Container(
-                  //  color: Color.fromARGB(159, 215, 243, 6),
+                    color: Color.fromARGB(132, 13, 10, 200),
                     child: Container(
-                        decoration: BoxDecoration(
+                      decoration: BoxDecoration(
                         image: DecorationImage(
-                          image: AssetImage('assets/1.png'),
+                          image: AssetImage('assets/3.png'),
                         ),
                       ),
                     ),
                   ),
                 ),
+                Expanded(
+                  child: Container(
+                    color: Color.fromARGB(159, 215, 243, 6),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/1.png'),
+                          
+                        ),
+                      ),
+
+
+
+                    ),
+                    
+                  ),
+                ),
               ],
             )),
             Expanded(
-                child: Row(
-              children: [
-                Container(
-                  child: 
-                    Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    child: Expanded(
                       child: Container(
                         color: Color.fromARGB(122, 4, 242, 56),
                         child: Container(
-                            decoration: BoxDecoration(
+                          decoration: BoxDecoration(
                             image: DecorationImage(
                               image: AssetImage('assets/4.png'),
                             ),
@@ -106,32 +88,88 @@ class homepage extends StatelessWidget {
                         ),
                       ),
                     ),
-                ),
-                
-            
-                
-                
-                    Expanded(
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Color.fromARGB(95, 228, 25, 11),
                       child: Container(
-                        color: Color.fromARGB(95, 228, 25, 11),
-                        child: Container(
-                            decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/2.png'),
-                            ),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/2.png'),
                           ),
                         ),
                       ),
                     ),
-                  ],
-                ),
-              
+                  ),
+                ],
+              ),
             ),
           ],
         ),
+        Center(
+         
+            child: Container(
+              height: 350,
+              width: 500,
+              // decoration: BoxDecoration(
+              //   borderRadius: BorderRadius.all(
+              //     Radius.circular(50),
+              //   ),
+              //   image: DecorationImage(
+              //     image: AssetImage('assets/5.png'),
+              //   ),
+              // ),
+              
+              child: Center(
+                child: _gret(),
+              ),
+              
+             
+              ),
+            ),
+            
+          
+          
         
       ],
-      
     );
   }
 }
+
+
+
+
+
+Widget _gret(){
+  return SizedBox(
+    width: 300,
+    child: DefaultTextStyle(
+      style: const TextStyle(
+        fontSize: 30.0,
+        fontWeight: FontWeight.w700,
+      ),
+      child: AnimatedTextKit(
+        isRepeatingAnimation: true,
+          animatedTexts: [
+              TyperAnimatedText('Hello Thear!'
+                ,speed: Duration(milliseconds: 150)),
+            TyperAnimatedText('This massige for Tuwaiq Academy'
+                ,speed: Duration(milliseconds: 150)),
+            TyperAnimatedText('Thank U All for what U gift Us in this Bootcamp'
+                ,speed: Duration(milliseconds: 150)),
+            TyperAnimatedText('Specifically coach Fahad, he is Aoswm :))'
+                ,speed: Duration(milliseconds: 150)),
+            TyperAnimatedText('  and i test this widjet to prove to u that im ready to learning more'
+                ,speed: Duration(milliseconds: 150)),
+                   TyperAnimatedText('Again Thank u All '
+                ,speed: Duration(milliseconds: 150)),
+                   TyperAnimatedText('– Abdullah Alhasain'
+                ,speed: Duration(milliseconds: 150)),
+          ]
+    ),
+  ),
+  );
+}
+
+
+
